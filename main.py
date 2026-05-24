c313e6c89557b3cd3fa568dd310ec7ccff7fb001
Tomas Martinez <tomasmartineztangarife@gmail.com>
2026-05-23 23:21:09 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def Peliculas(request: Request):
+    return templates.TemplateResponse(request=request, name="pelicula.html", context={"pelicula": "Interestelar", "director": "Christopher Nolan", "anio": 2014, "nombre": "Tomás"})
+
+
+colores = ["Red", "Blue", "Green", "Yellow", "Black"]
+
+@app.get("/colores")
+async def Colores(request: Request):
+    return templates.TemplateResponse(request=request, name="colores.html", context={"colores": colores, "nombre": "Tomás"})
+
+equipos = {
+
+    "Nacional" : {
+        "color": "#A8E6A3",
+        "jugadores": [
+        "David Ospina",
+        "Andrés Felipe Román",
+        "Jorman Campuzano",
+        "Andrés Sarmiento",
+        "Eduard Bello"
+        ]
+    },
+
+    "Independiente Medellín" : {
+        "color": "#FFB3B3",
+        "jugadores": [
+        "Eder Chaux",
+        "José Ortiz",
+        "Kevin Mantilla",
+        "Frank Fabra",
+        "Baldomero Perlaza"
+        ]
+    },
+
+    "Junior" : {
+        "color": "#A7C7E7",
+        "jugadores": [
+        "Mauro Silveira",
+        "Mauro Silveira",
+        "Juan David Ríos",
+        "Jannenson Sarmiento",
+        "Bryan Castrillón"
+        ]
+    }
+}
+
+@app.get("/equipos")
+async def mostrar_equipos(request: Request):
+    return templates.TemplateResponse(request=request, name="equipos.html", context={"equipos": equipos, "nombre": "Tomás"})
